15a6d674960bcb3def066a82cd8299783e477ac5
oandreazini <oriol_as_92@hotmail.com>
2022-08-05 12:30:33 +0200

    structure calculator

---
+++ TA21-Calculator/src/main/java/TA21_Calculator/TA21_Calculator/App.java
@@ -0,0 +1,13 @@
+package TA21_Calculator.TA21_Calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ TA21-Calculator/src/test/java/TA21_Calculator/TA21_Calculator/AppTest.java
@@ -0,0 +1,20 @@
+package TA21_Calculator.TA21_Calculator;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
