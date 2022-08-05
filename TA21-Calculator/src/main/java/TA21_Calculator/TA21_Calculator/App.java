ae51f338404ee363d5dbe5aa2f6c56b816dff6e7
rrrcoba <roger-decroly@hotmail.com>
2022-08-05 14:15:15 +0200

    Initial structure of the calculator

--- TA21-Calculator/src/main/java/TA21_Calculator/TA21_Calculator/App.java
+++ TA21-Calculator/src/main/java/TA21_Calculator/TA21_Calculator/App.java
@@ -1,13 +1,7 @@
 package TA21_Calculator.TA21_Calculator;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		Window window = new Window();
+	}
 }
