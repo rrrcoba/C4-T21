1b9f806b3a1548dd71d18b5302439579f2f05523
rrrcoba <roger-decroly@hotmail.com>
2022-08-10 19:18:12 +0200

    Tests completados para power2, sqrt, divide y signChange.

--- TA21-Calculator/src/test/java/TA21/Calculator/dto/CalculadoraTest.java
+++ TA21-Calculator/src/test/java/TA21/Calculator/dto/CalculadoraTest.java
@@ -6,13 +6,14 @@ import org.junit.jupiter.api.Test;
 
 class CalculadoraTest {
 	
+	Operations operations = new Operations();
+	
 	/**
 	 * *feature*: (sobre la qual fem @Test): "plus" de la classe "Operations"
 	 * *given*: classe Operations
 	 */
 	@Test
-	void testAreaCuadrado() {
-		Operations operations = new Operations();
+	void testPlus() {
 		String num1 = "2";
 		String num2 = "2";
 		
@@ -21,5 +22,68 @@ class CalculadoraTest {
 		Double resultCalculated = operations.plus(num1, num2);
 		assertEquals(resultExpected, resultCalculated);
 	}
-
+	
+	
+	/**
+	 * *feature*: (sobre la qual fem @Test): "power2" de la classe "Operations"
+	 * *given*: classe Operations
+	 */
+	@Test
+	void testPower2() {
+		String num1 = "2";
+		
+		
+		Double resultExpected = 4.0;
+		Double resultCalculated = operations.power2(num1);
+		assertEquals(resultExpected, resultCalculated);
+	}
+	
+	/**
+	 * *feature*: (sobre la qual fem @Test): "sqrt" de la classe "Operations"
+	 * *given*: classe Operations
+	 */
+	@Test
+	void testSqrt() {
+		String num1 = "4";
+		
+		
+		Double resultExpected = 2.0;
+		Double resultCalculated = operations.sqrt(num1);
+		assertEquals(resultExpected, resultCalculated);
+	}
+	
+	/**
+	 * *feature*: (sobre la qual fem @Test): "divide" de la classe "Operations"
+	 * *given*: classe Operations
+	 */
+	@Test
+	void testDivide() {
+		String num1 = "4";
+		String num2 = "2";
+		
+		
+		Double resultExpected = 2.0;
+		Double resultCalculated = operations.divide(num1, num2);
+		assertEquals(resultExpected, resultCalculated);
+	}
+	
+	/**
+	 * *feature*: (sobre la qual fem @Test): "signChange" de la classe "Operations"
+	 * *given*: classe Operations
+	 */
+	@Test
+	void testSignChange() {
+		String num1 = "-200";
+		
+		// Con signo negativo
+		String resultExpected = "200";
+		String resultCalculated = operations.signChange(num1);
+		assertEquals(resultExpected, resultCalculated);
+		
+		// Sin signo
+		num1 = "-100.0";
+		resultExpected = "100.0";
+		resultCalculated = operations.signChange(num1);
+		assertEquals(resultExpected, resultCalculated);
+	}
 }
